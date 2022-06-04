d1345e38ddd8317127da2feaca7cd1f1008db1e4
Ganesh Navale <navaleganesh2011@gmail.com>
2022-06-04 22:20:14 +0530

    Added meals component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 function App() {
   return (
     <div>
       <Header />
-      <h2>Let's get started!</h2>
+      <main>
+        <Meals />
+      </main>
     </div>
   );
 }
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import AvailableMeals from "./AvailableMeals";
+import MealsSummary from "./MealsSummary";
+import { Fragment } from "react";
+const Meals = (props) => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailableMeals />
+    </Fragment>
+  );
+};
+
+export default Meals;
