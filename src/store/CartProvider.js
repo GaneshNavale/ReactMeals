e71011b3cd364c963486aeddb282023d72f61c78
Ganesh Navale <navaleganesh2011@gmail.com>
2022-06-05 14:44:52 +0530

    Worked on logic to add item for already added items.

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -4,9 +4,24 @@ import { useReducer } from "react";
 const defaultCartState = { items: [], totalAmount: 0 };
 const cartReducer = (state, action) => {
   if (action.type === "ADD_ITEM") {
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
     return { items: updatedItems, totalAmount: updatedTotalAmount };
   }
   if (action.type === "REMOVE_ITEM") {
