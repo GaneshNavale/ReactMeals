96e09a11298a56165318e55e3818b10d15dbe152
Ganesh Navale <navaleganesh2011@gmail.com>
2022-06-05 13:44:35 +0530

    Adding Item to cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,7 +3,7 @@ import classes from "./Cart.module.css";
 
 const Cart = (props) => {
   const cartItems = [
-    { id: "c1", name: "Sushi", quantity: "2", price: 12.99 },
+    { id: "c1", name: "Sushi", amount: "2", price: 12.99 },
   ].map((item) => <li>{item.name}</li>);
   return (
     <Modal onCloseCart={props.onCloseCart}>
